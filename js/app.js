17a12682fc7586911fae6e9b2cb492b5a417591e
Natalia Podosinnikova <Natalia@natalias-air.attlocal.net>
2016-03-22 06:32:05 -0500

    cleaning up any staged and uncomitted changes

--- js/app.js
+++ js/app.js
@@ -39,7 +39,87 @@ import React, {Component} from 'react'
 function app() {
     // start app
     // new Router()
-    DOM.render(<p>test 2</p>, document.querySelector('.container'))
+        var AppView = React.createClass ({ 
+        render: function() { 
+        //console.log(the AppView is...)
+        //console.log(this)
+    		return (
+    			<div className="pageContainer">	
+	    			<img className="tardis" src="../dist/images/tardis.png"/>
+	    			<Console/>
+	    		</div>	
+    		)
+    	}
+    })
+
+        var Console = React.createClass ({
+
+        	_forward: function() {
+        		if (!this.state.ticking) {
+            		var forwardYear = function() {
+              			 this.setState({ 
+                 		 year: this.state.year + 1,
+                  		 forwardSymbol: "\u23f8", 
+                  			ticking: true,                  
+              			 })
+           			}
+          		var boundIncrementer = forwardYear.bind(this)
+          		this.intervalId = setInterval(boundIncrementer,500)
+        		}		
+
+        		else {
+          			clearInterval(this.intervalId)
+          			this.setState({
+            		forwardSymbol: '\u23E9',
+            		ticking: false
+          			})
+        		}
+        
+      		},
+
+      		_rewind: function() {
+				if (!this.state.ticking) {
+            		var rewindYear = function() {
+              			 this.setState({ 
+                 		 year: this.state.year - 1,
+                  		 rewindSymbol: "\u23f8", 
+                  			ticking: true,                  
+              			 })
+           			}
+          		var boundIncrementer = rewindYear.bind(this)
+          		this.intervalId = setInterval(boundIncrementer,500)
+        		}		
+
+        		else {
+          			clearInterval(this.intervalId)
+          			this.setState({
+            		rewindSymbol: '\u23ea',
+            		ticking: false
+          			})
+        		}
+
+      		},
+
+        	getInitialState: function() { 
+        		return {
+          			year: 2016,
+          			forwardSymbol: "\u23E9",
+          			rewindSymbol: "\u23ea", 
+          			ticking: false
+        		}
+      		},
+
+        	render: function() {
+        		return (
+        			<div className="console">
+        				
+        				<p className="year"><button className="rewindButton" onClick={this._rewind}>{this.state.rewindSymbol}</button>{this.state.year}<button className="forwardButton" onClick={this._forward}>{this.state.forwardSymbol}</button></p>
+        			
+        			</div>
+        			)
+        	}
+        })
+    DOM.render(<AppView/>, document.querySelector('.container'))
 }
 
 app()
